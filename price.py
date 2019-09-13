9268d1b8119d6557159cba7a4ab17e3d72f855b0
Dmitry Fedorov <fdmoscow@gmail.com>
2019-09-13 22:36:17 +0800

    Функция 2 задания

--- price.py
+++ price.py
@@ -1,7 +1,23 @@
-def get_summ(one, two, delimiter = '&'):
-    one = (str(one))
-    two = (str(two))
-    delimiter = (one & two)
-    print (delimiter)
+#Функции Задание 1
 
-get_summ(one, two)
+def get_summ(one, two, delimiter='&'):
+
+    print(str.upper('learn ' + delimiter + ' python')) 
+    
+        
+get_summ('learn', 'python')
+    
+
+#Функция Задание 2
+
+def format_price(price):
+    return 'Цена: ЧИСЛО  руб' 
+    print(int(price))
+    
+x = 56.24
+format_price(x)
+print(x)
+
+
+
+
