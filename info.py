399475aee3a1e2ce55ef4430ec0e613c4d90a9a4
Dmitry Fedorov <fdmoscow@gmail.com>
2019-09-16 22:58:51 +0800

    Упростил код, показал, что выводит

--- info.py
+++ info.py
@@ -1,3 +1,6 @@
-dict_user_name = {'first_name': 'Дмитрий', 'last_name': 'Федоров'} #не уверен, что нужно указывать dict, без него тоже работает.
-x = dict_user_name['first_name']
-print(x)
+
+#Переменные, 1 неделя "Информация о пользователе"
+
+user_name = {'first_name': 'Дмитрий', 'last_name': 'Федоров'} #не уверен, что нужно указывать dict, без него тоже работает.
+x = user_name['first_name']
+print(x) #Дмитрий
