eb0132e9586ee7a8a0aa4141e23914688a1c19dc
Dmitry Fedorov <fdmoscow@gmail.com>
2019-09-16 14:12:40 +0800

    убрал пробелы

--- dict.py
+++ dict.py
@@ -2,14 +2,14 @@ name = {"city": "Москва", "temperature": 20}
 
 print(name['city'])
 
-name ['temperarure']= 15
+name['temperarure']= 15
 
 print(name)
 
-print (name.get ('country'))
+print(name.get ('country'))
 
-print (name.get ('country', 'Россия'))
+print(name.get ('country', 'Россия'))
 
-name ['date']= '27.05.2019'
+name['date']= '27.05.2019'
 
 print(len(name))
